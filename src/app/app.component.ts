26e7feba9e33988749c483d019dc4f3709e2a4ae
Justice Sam <fjaccobian@gmail.com>
2024-08-24 22:52:31 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { NgFor } from '@angular/common';
+import { ProductService } from './services/services';
+import {
+  PageResponseProductResponse,
+  ProductResponse,
+} from './services/models';
+import { NewProductComponent } from './new-product/new-product.component';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, ProductComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'product-ui';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AddProductFormComponent } from './add-product-form/add-product-form.component';
+import { NewProductComponent } from './new-product/new-product.component';
+import { SingleProductComponent } from './single-product/single-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'add-product',
+    component: AddProductFormComponent,
+  },
+  {
+    path: 'new-product',
+    component: NewProductComponent,
+  },
+  {
+    path: 'single',
+    component: SingleProductComponent,
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+  },
+];
